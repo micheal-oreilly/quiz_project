6774a26b7d22b6f680fe88c7b0c8ae145a7b5c99
micheal-oreilly <48756366+micheal-oreilly@users.noreply.github.com>
2021-03-24 15:33:22 +0100

    24/3/2021

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
-class User:
+# class User:
+#
+#     def __init__(self, user_id, username):
+#         self.id = user_id
+#         self.username = username
+#         self.followers = 0
+#         self.following = 0
+#         print("New user created...")
+#
+#     def follow(self, user):
+#         user.followers += 1
+#         self.following += 1
+#
+#
+# user_1 = User("001", "Mihi")
+# user_2 = User("002", "Bobby")
+#
+# user_1.follow(user_2)
+#
+# print(user_1.followers)
+# print(user_1.following)
+# print(user_2.followers)
+# print(user_2.following)
 
-    def __init__(self, user_id, username):
-        self.id = user_id
-        self.username = username
-        self.followers = 0
-        self.following = 0
-        print("New user created...")
 
-    def follow(self, user):
-        user.followers += 1
-        self.following += 1
+from question_model import Question
+from data import question_data
 
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-user_1 = User("001", "Mihi")
-user_2 = User("002", "Bobby")
-
-user_1.follow(user_2)
-
-print(user_1.followers)
-print(user_1.following)
-print(user_2.followers)
-print(user_2.following)
+print(question_bank)
