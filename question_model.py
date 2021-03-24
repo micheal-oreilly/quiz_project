39ce64233f6a6f3ca5d9d56d9788a17e21932cec
micheal-oreilly <48756366+micheal-oreilly@users.noreply.github.com>
2021-03-24 15:18:45 +0100

    24/3/2021

---
+++ question_model.py
@@ -0,0 +1,17 @@
+class Question:
+
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+
+
+
+
+    #     self.followers = 0
+    #     self.following = 0
+    #     print("New user created...")
+    #
+    # def follow(self, user):
+    #     user.followers += 1
+    #     self.following += 1
