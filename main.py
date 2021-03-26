95add7b9223a44b0c299aab33fc663421597dc09
micheal-oreilly <48756366+micheal-oreilly@users.noreply.github.com>
2021-03-26 15:12:33 +0100

    26/3/2021

--- main.py
+++ main.py
@@ -25,6 +25,7 @@
 
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -33,7 +34,5 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-print(question_bank[0].text)
-print(question_bank[0].answer)
-print(question_bank[1].text)
-print(question_bank[1].answer)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
--- quiz_brain.py
+++ quiz_brain.py
@@ -4,11 +4,14 @@
 
 #TODO: checking if the quiz is over
 
-class Quizbrain:
+class QuizBrain:
 
-    def __init__(self, questions_list):
+    def __init__(self, question_list):
         self.question_number = 0
-        self.question_list = questions_list
+        self.question_list = question_list
 
-    def next_question():
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number +=1
+        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
 
