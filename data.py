7bdac9df20af83927b7d4a3378185ca4a4f99e2e
micheal-oreilly <48756366+micheal-oreilly@users.noreply.github.com>
2021-03-30 15:01:30 +0200

    30/3/2021

--- data.py
+++ data.py
@@ -14,3 +14,46 @@ question_data = [
     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
     {"text": "A few ounces of chocolate can kill a small dog.", "answer": "True"}
 ]
+question_data2 = [
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The main character in the &quot;Half-Life&quot; franchise is named Morgan Freeman.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "The Great Wall of China is visible from the moon.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Geography", "type": "boolean", "difficulty": "easy",
+     "question": "Greenland is almost as big as Africa.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Board Games", "type": "boolean", "difficulty": "easy",
+     "question": "The card game &quot;Munchkin&quot; won the 2001 Origins Award for Best Traditional Card Game.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Cartoon & Animations", "type": "boolean",
+     "difficulty": "easy",
+     "question": "Waylon Smithers from &quot;The Simpsons&quot; was originally black when he first appeared in the series.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Geography", "type": "boolean", "difficulty": "easy",
+     "question": "Hungary is the only country in the world beginning with H.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean",
+     "difficulty": "easy",
+     "question": "In the &quot;To Love-Ru&quot; series, Golden Darkness is sent to kill Lala Deviluke.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Dihydrogen Monoxide was banned due to health risks after being discovered in 1983 inside swimming pools and drinking water.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "Entertainment: Music",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Lead Singer Rivers Cuomo of American rock band Weezer attended Harvard.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]}
+]
